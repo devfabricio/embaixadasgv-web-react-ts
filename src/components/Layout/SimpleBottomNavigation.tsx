9015955c3b335c306fc46d0e1f6091668a59fed2
Fabrício Augusto <fabricio.mata@gmail.com>
2019-10-03 00:34:24 -0300

    Iniciado a página principal do app

--- src/components/Layout/SimpleBottomNavigation.tsx
+++ src/components/Layout/SimpleBottomNavigation.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Home from '@material-ui/icons/Home'
+import Group from '@material-ui/icons/Group'
+import Description from '@material-ui/icons/Description'
+import EventAvailable from '@material-ui/icons/EventAvailable'
+import Menu from '@material-ui/icons/Menu'
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const useStyles = makeStyles({
     root: {
-        width: 500,
+        width: "100%",
     },
 });
 
@@ -25,9 +30,11 @@ export default function SimpleBottomNavigation() {
             showLabels
             className={classes.root}
         >
-            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+            <BottomNavigationAction label="Início" icon={<Home />} />
+            <BottomNavigationAction label="GV's" icon={<Group />} />
+            <BottomNavigationAction label="Conteúdo" icon={<Description />} />
+            <BottomNavigationAction label="Agenda" icon={<EventAvailable />} />
+            <BottomNavigationAction label="Mais" icon={<Menu />} />
         </BottomNavigation>
     );
 }
--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
 import SimpleBottomNavigation from "../Layout/SimpleBottomNavigation";
+import {Link} from "react-router-dom";
+import PermanentDrawerLeft from "../Layout/Drawer";
 
 class MainDashboard extends Component{
     render() {
         console.log("MainDashboard", "chegou aqui")
 
         return(
-            <div>
-                <SimpleBottomNavigation/>
+            <div className={"app-wrap"}>
+                <div className={"container"}>
+                    <PermanentDrawerLeft/>
+                </div>
             </div>
         )
     }
